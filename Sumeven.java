0b17fa7a2d53a0f59c33e3c21cfa2f2779d50b00
jaysolanki8 <73906363+jaysolanki8@users.noreply.github.com>
2020-11-23 02:23:21 +0530

    Add files via upload

---
+++ Sumeven.java
@@ -0,0 +1,11 @@
+public class Sumeven{
+public static void main(String[] args){
+int i=0, sum=0, count=0;
+do{
+sum=sum+i;
+i=i+2;
+count=count+1;
+}while(count<=10);
+System.out.println("Sum of first 10 even numbers is "+sum);
+}
+}
